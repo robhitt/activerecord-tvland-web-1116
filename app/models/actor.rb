3a72c86bd0258ee1aaa21256d7871275aa398063
Rob Hitt <robhitt@gmail.com>
2016-12-13 16:10:17 -0500

    Done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  #can list all of its shows and characters (FAILED - 1)
+  def list_roles
+    #{#{character_name} - #{show_name}"
+    roles_array = []
+
+    #"Khaleesi - Game of Thrones"
+    self.characters.each do |character|
+      self.shows.each do |show|
+        roles_array << "#{character.name} - #{show.name}"
+      end
+    end
+
+    roles_array
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show
   
+
+  def say_that_thing_you_say
+    #say his name and catchphrase
+    #remember the model is yours do with as you please, you
+    #are free to add methods that perform actions on the
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+
 end
